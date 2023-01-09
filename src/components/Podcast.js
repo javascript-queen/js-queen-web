a3d736a0ea92958c21aa69630187a0dcc1fe51d5
Mrs. Mars <dev@annagradient.com>
2023-01-09 13:15:29 -0600

    feat: delete faq section

--- src/components/Podcast.js
+++ src/components/Podcast.js
@@ -1,12 +1,11 @@
-import React, {Component} from 'react'
+import React from 'react'
 import './Podcast.css'
 
-export class Podcast extends Component {
-  render(){
+const Podcast = (props) => {
     return (
       <div className='hero-podcast'>
       <div className='content-podcast'>
-        <h1>{this.props.heading}</h1>
+        <h1>{props.heading}</h1>
         <div className='buttons'>
         <a href='https://anchor.fm/js-abuse' className='btn-light btn-podcast' style={{color: "#FFFFFF"}} target="_blank" rel="noreferrer" >Podcast Website</a>
         <a href='https://medium.com/@js-queen' className='btn-light btn-podcast' style={{color: "#FFFFFF"}} target="_blank" rel="noreferrer" >Medium</a>
@@ -15,6 +14,5 @@ export class Podcast extends Component {
     </div>
     )
   }
-}
 
 export default Podcast
--- src/components/PodcastTwo.js
+++ src/components/PodcastTwo.js
@@ -1,12 +1,11 @@
 import './PodcastTwo.css'
-import React, {Component} from 'react'
+import React from 'react'
 
-export class PodcastTwo extends Component {
-  render() {
+const PodcastTwo = (props) => {
     return (
       <div className='hero-podcast-two'>
       <div className='content-podcast-two'>
-        <h1>{this.props.headingtwo}</h1>
+        <h1>{props.headingtwo}</h1>
         <div className='buttons'>
         <a href='https://anchor.fm/programmersreadbooks' className='btn btn-podcast-two' style={{color: "#2E194B"}} target="_blank" rel="noreferrer" >Podcast Website</a>
         <a href='https://t.me/programmersreadbooks' className='btn btn-podcast-two' style={{color: "#2E194B"}} target="_blank" rel="noreferrer" >Telegram</a>
@@ -15,6 +14,5 @@ export class PodcastTwo extends Component {
     </div>
     )
   }
-}
 
 export default PodcastTwo
