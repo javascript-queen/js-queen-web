e8056aef689d8eb68d99630639305f6ecce7e37c
Mrs. Mars <dev@annagradient.com>
2023-01-09 13:52:03 -0600

    feat: update button

--- src/components/Podcast.js
+++ src/components/Podcast.js
@@ -7,7 +7,7 @@ const Podcast = (props) => {
         <div className='content-podcast-two'>
           <h1>{props.heading}</h1>
           <div className='buttons'>
-            <a href='https://anchor.fm/js-abuse' className='btn btn-podcast-two' style={{color: "#2E194B"}} target="_blank" rel="noreferrer" >JS Abuse</a>
+            <a href='https://anchor.fm/js-abuse' className='btn btn-podcast-two' style={{color: "#2E194B"}} target="_blank" rel="noreferrer" >JavaScript Abuse</a>
             <a href='https://anchor.fm/programmersreadbooks' className='btn-light btn-podcast-two' style={{color: "#FFFFFF"}} target="_blank" rel="noreferrer" >Programmers Read Too</a>
           </div>
       </div>
